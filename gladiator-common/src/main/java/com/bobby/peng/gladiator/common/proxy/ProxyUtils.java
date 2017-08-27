75badf4f823c23042893352b47a6c38da35009ee
peng2035@163.com <peng2035@163.com>
2017-08-27 23:39:00 +0800

    add proxy

---
+++ gladiator-common/src/main/java/com/bobby/peng/gladiator/common/proxy/ProxyUtils.java
@@ -0,0 +1,34 @@
+package com.bobby.peng.gladiator.common.proxy;
+
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by bobby.peng on 2017/8/27.
+ */
+public class ProxyUtils {
+
+    public static void generateClassFile(Class clazz,String proxyName){
+        byte[] classFile = ProxyGenerator.generateProxyClass(proxyName, clazz.getInterfaces());
+        String paths = clazz.getResource(".").getPath();
+        System.out.println(paths);
+        FileOutputStream out = null;
+
+        try {
+            out = new FileOutputStream(paths+proxyName+".class");
+            out.write(classFile);
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
